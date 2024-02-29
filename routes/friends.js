a92f30b8b7ea60ef88e66a3bae86ad86b5e787db
nargizskakova-04 <skakovasaule7@gmail.com>
2024-02-29 13:19:58 +0600

    Styling red

--- routes/friends.js
+++ routes/friends.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user'); 
 
 router.get('/friends', async (req, res) => {
-    try {
-      res.render('friends', { 
-        user: req.user
-      });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Ошибка сервера');
+  try {
+    if (!req.user) {
+      return res.redirect('/login'); 
     }
-  });
+
+    const users = await User.find({});
+    res.render('friends', { 
+      user: req.user, 
+      users: users 
+    });
+  } catch (error) {
+    console.error('Ошибка при получении списка пользователей:', error);
+    res.status(500).send('Ошибка сервера');
+  }
+});
 
 module.exports = router;
